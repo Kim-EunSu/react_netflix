1e6e66e843f41c5de16b1110b7ef6c41e0106a55
Kim-EunSu <kes73640@gmail.com>
2022-12-21 10:58:12 +0900

    useWindowDimensions 겹침현상해결

--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import { motion, AnimatePresence } from "framer-motion";
+import useWindowDimensions from "./useWidowDimensions";
 
 const SWrapper = styled.div`
   position: relative;
@@ -23,36 +24,43 @@ const Box = styled(motion.div)`
   font-size: 66px;
 `;
 
-const rowVariants = {
-  hidden: {
-    x: window.outerWidth + 10,
-  },
-  visible: {
-    x: 0,
-  },
-  ecit: {
-    x: -window.outerWidth - 10,
-  },
-};
-
 function Slider() {
+  //겹침현상 해결해주는 함수
+  const width = useWindowDimensions();
+
   // Slider의 다음 페이지로 넘어갈수 있게 하는 기능 만들기(index때문에)
   const [index, setIndex] = useState(0);
 
   // index를 증가시키는 함수
-  const increaseIndex = () => setIndex((prev) => prev + 1);
+  const IncreaseIndex = () => {
+    // 음 클릭할때는 leaving이 false이겠지만 거기서 leaving을 true로 바꾼 다음 index를 증가시키기
+    if (leaving) return;
+    setLeaving(true);
+    setIndex((prev) => prev + 1);
+  };
+
+  // Row를 클릭했을때 겹치는 현상 없애주는 함수
+  const [leaving, setLeaving] = useState(false);
+
+  // onExitComplete(exit끝났을때 실행)를 실행시키는 함수
+  const toggleLeaving = () => setLeaving((prev) => !prev);
 
   return (
     <>
       <SWrapper>
-        <AnimatePresence>
+        {/* toggleLeaving을 호출하면 leaving은 true가 되고 exit이 끝났을 때 toggleLeaving을 호출하면 leaving은 false가 됨 */}
+        {/* initial={false}를 줌으로써 제자리에서 움직임 */}
+        <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
           <Row
-            onClick={increaseIndex}
-            variants={rowVariants}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
+            //variants={rowVariants}
+            // initial="hidden"
+            // animate="visible"
+            // exit="exit"
+            initial={{ x: width + 10 }}
+            animate={{ x: 0 }}
+            exit={{ x: -width - 10 }}
             transition={{ type: "tween", duration: 1 }}
+            onClick={IncreaseIndex}
             key={index}
           >
             {[1, 2, 3, 4, 5, 6].map((el) => (
